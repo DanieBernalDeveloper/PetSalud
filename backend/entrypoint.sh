c7f72eac99ed00e9b18c631ef5513534a5a32664
DanieBernalDeveloper <bernalarizad7@gmail.com>
2025-11-10 22:09:50 -0500

    Arreglo backend (problemas de compatibilidad)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-echo "⏳ Esperando a que la base de datos esté lista..."
+if [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ]; then
+  echo "❌ Variables DB_HOST o DB_PORT no están definidas"
+  exit 1
+fi
+
+echo "⏳ Esperando a que la base de datos esté lista en $DB_HOST:$DB_PORT..."
 while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
 echo "✅ Base de datos disponible"
 
 echo "📦 Ejecutando migraciones..."
-python manage.py migrate --noinput
+python ./app/manage.py migrate --noinput
 
 echo "🚀 Iniciando servidor Django..."
-python manage.py runserver 0.0.0.0:8000
+python ./app/manage.py runserver 0.0.0.0:8000
